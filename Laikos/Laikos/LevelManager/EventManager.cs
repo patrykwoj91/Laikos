cd124a757480e83a50e0c3e51fd21d966fae6ea8
khazad123 <wasilewski.pio@gmail.com>
2013-04-16 17:39:06 +0200

    events

---
+++ Laikos/Laikos/LevelManager/EventManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace Laikos
+{
+    public class EventManager : GameComponent
+    {
+        enum Events
+        {
+            FixCollisions,
+            ScaleUp,
+            Heal
+        };
+
+        List<Message> messages;
+
+
+        public EventManager(Game game) : base(game)
+        {
+            messages = new List<Message>();
+        }
+
+        public void CreateMessage(Message message)
+        {
+            messages.Add(message);
+        }
+
+        public void FindMessageByDestination(GameObject destinationObject, List<Message> result)
+        {
+            for (int i = 0; i < result.Count; i++)
+
+        }
+
+
+
+        public override void Update(GameTime gameTime)
+        {
+            
+            base.Update(gameTime);
+        }
+    }
+}
