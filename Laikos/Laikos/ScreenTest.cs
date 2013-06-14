fdb4926f23629a5521c3a3f5659f88146fbc46f0
khazad123 <wasilewski.pio@gmail.com>
2013-06-14 13:07:36 +0200

    screentest

--- Laikos/Laikos/ScreenTest.cs
+++ Laikos/Laikos/ScreenTest.cs
@@ -9,7 +9,7 @@ namespace UI
         // create a graphic in the centre of the screen that is the size of the safe area
         WidgetGraphic g = new WidgetGraphic();
         g.Position = new Vector3(_UI.SXM, _UI.SYM, 0.0f);
-        g.Size = new Vector3(_UI.SSX, _UI.SSY, 0.0f);
+        g.Size = new Vector3(100, 200, 0.0f);
         g.ColorBase = Color.White;
         g.Align = E_Align.MiddleCentre;
         g.AddTexture("null", 0.0f, 0.0f, 1.0f, 1.0f);
