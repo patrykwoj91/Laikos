5329b187cd73c13fa6741a5feb0e0801f95ec7bd
Piotr 'Zielu' Zieliński <zielu.yabi@gmail.com>
2013-06-16 02:40:06 +0200

    Budynki nie działają............

---
+++ MyDataTypes/Serialization/DecorationSchema.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyDataTypes.Serialization
+{
+    public class DecorationSchema
+    {
+        public String name;
+        public float x;
+        public float y;
+
+        public DecorationSchema()
+        {
+            name = "";
+            x = 0;
+            y = 0;
+        }
+
+        public DecorationSchema(String _name, float _x, float _y)
+        {
+            name = _name;
+            x = _x;
+            y = _y;
+        }
+
+        public DecorationSchema(String _name)
+        {
+            name = _name;
+            x = 0;
+            y = 0;
+        }
+    }
+}
